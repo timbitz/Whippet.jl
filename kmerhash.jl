e76aaad1a6d3e361550956e54f38b60f8d44abd4
Tim Sterne-Weiler <tim.sterne.weiler@utoronto.ca>
2015-11-12 15:55:33 -0500

    Update kmerhash.jl

--- kmerhash.jl
+++ kmerhash.jl
@@ -5,22 +5,20 @@ using ArgParse
 typealias Str ASCIIString
 
 # overload some stuff here
-# lets hash on identity for speed 
+# lets hash on numeric value for speed 
 Base.hash{T <: Bio.Seq.Kmer}( h::T ) = UInt64(h)
 
 d = Dict{Kmer,Str}() # set the hash
-n = 3000000000 # should be ~ 3billion kmers in the human genome
+n = 1000000000 # should be ~ 3billion kmers in the human genome
 sizehint!(d, n)
 
 t32 = typeof(DNAKmer(join(["A" for i in 1:32], "")))
 
 # This benchmarks faster than BloomFilters.jl
-'''
-@time for i in 1:2n
+@time for i in 1:n
   if i % 1000000 == 0
     println(i)
   end
   k = convert(t32, UInt64(i))
   d[k] = "Gene1"
 end
-'''
