900284ef1518bbd6a40e5528db86d688faa6550a
Tim Sterne-Weiler <tim.sterne.weiler@utoronto.ca>
2015-11-13 17:16:49 -0500

    Update setup.jl

--- setup.jl
+++ setup.jl
@@ -1,10 +1,14 @@
 #!/usr/bin/env julia
 
-pkgs = ["ArgParse", "Bio", "Glob", "GZip"]
+pkgs = ["ArgParse", "Bio", "FMIndexes", "BitArrays", "GZip"]
 map( x->Pkg.add(x), pkgs )
 Pkg.update()
+map( x->Pkg.test(x), pkgs )
 
 using Bio.Seq
+using FMIndexes
+using BitArrays
 using ArgParse
-using Glob
 using GZip
+
+println(STDERR, "looks okay!")
