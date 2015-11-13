c14f621c18cfed6bc7c08e9125f1bd464ceb4b8a
Tim Sterne-Weiler <tim.sterne.weiler@utoronto.ca>
2015-11-12 19:22:53 -0500

    Update kmerhash.jl

--- kmerhash.jl
+++ kmerhash.jl
@@ -1,6 +1,5 @@
 
 using Bio.Seq
-using ArgParse
 
 typealias Str ASCIIString
 
@@ -8,13 +7,8 @@ typealias Str ASCIIString
 # lets hash on numeric value for speed 
 Base.hash{T <: Bio.Seq.Kmer}( h::T ) = UInt64(h)
 
-df = Dict{DNAKmer{32},Str}() # set the hash
-d = Dict{DNAKmer{32},Str}()
-n = 3000000000 # should be ~ 3billion kmers in the human genome
-@time succ_rehash!(d, n)
-
-function succ_rehash!(d::Dict, targsz::Integer)
-  seed = 1000000
+# this benchmarks way faster than one initial sizehint! for somereason
+function succ_rehash!(d::Dict, targsz::Integer; seed = Int(floor(targsz / 1000)))
   if targsz <= seed
     return
   else
@@ -27,10 +21,8 @@ function succ_rehash!(d::Dict, targsz::Integer)
 end
 
 # This benchmarks faster than BloomFilters.jl
-# without sizehint:  16.759637 seconds (263.34 M allocations: 6.512 GB, 27.96% gc time)
-# with sizehint:
 function set_hash!(d::Dict, n::Integer)
-  for i in 1:(n >> 4)
+  for i in 1:(n >> 2)
     if i % 1000000 == 0
       println(i)
     end
@@ -39,5 +31,7 @@ function set_hash!(d::Dict, n::Integer)
   end
 end
 
+d = Dict{DNAKmer{32},Str}()
+n = 3 * 1000000000 # should be ~ 3billion kmers in the human genome
+@time succ_rehash!(d, n, seed=30000000)
 @time set_hash!(d, n)
-@time set_hash!(df, n)
