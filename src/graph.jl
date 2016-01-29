60fc5784d34ba7c8e8f668b4c5d5fe4bb81d7040
Tim Sterne-Weiler <tim.sterne.weiler@utoronto.ca>
2016-01-29 12:56:58 -0500

    added edgetype and format for splice graph spec

--- src/graph.jl
+++ src/graph.jl
@@ -2,6 +2,10 @@
 using IntervalTrees
 
 typealias Exonmax UInt16
+typealias Edgetype UInt8 # Encode Edgetype as:
+# 'L' = 0x05
+# 'R' = 0x06
+# 'S' = 0x07, therfore... 'SL' = 0x07-0x05 = 0x02, 'SR' = 0x01
 
 # This is where we count reads for nodes/edges/circular-edges
 type SpliceGraphQuant 
@@ -24,15 +28,29 @@ immutable SpliceGraph
   left::Vector{Exonmax} #donor splice sites indexes
   right::Vector{Exonmax} #acceptor splice site indexes
   txend::Vector{Exonmax}  #tx starts/ends
+  seq::DNASequence
 end
 
+# empty constructor
 SpliceGraph() = SpliceGraph( Vector{Coordint}(), Vector{Coordint}(),
-                             Vector{Exonmax}(), Vector{Exonmax}(),
+                             Vector{Exonmax}(),  Vector{Exonmax}(),
                              Vector{Exonmax}() )
 
-function SpliceGraph( gene::Refgene )
-   ss = SpliceGraph()
-   for d in gene.don
+# Main constructor
+# Build splice graph here.
+function SpliceGraph( gene::Refgene, chrom::DNASequence )
+   # splice graph variables
+   nodeoffset = Vector{Coordint}()
+   nodesize   = Vector{Coordint}()
+   edgeoffset = Vector{Coordint}()
+   edgetype   = Vector{Edgetype}()
+   seq        = DNASequence()
+   
 
-   end  
+   for i in 1:length(don)
+      # iterate through donors, and acceptors
+      # left to right. '-' = rc unshift?
+   end
+   return SpliceGraph( nodeoffset, edgeoffset, 
+                       left, right, txend, seq )
 end
