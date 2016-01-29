e31a868f60457c68c9edf90871542097b83b4de5
Tim Sterne-Weiler <tim.sterne.weiler@utoronto.ca>
2016-01-29 16:42:19 -0500

    added EdgeType and updated SpliceGraph entries

--- src/graph.jl
+++ src/graph.jl
@@ -2,10 +2,25 @@
 using IntervalTrees
 
 typealias Exonmax UInt16
-typealias Edgetype UInt8 # Encode Edgetype as:
-# 'L' = 0x05
-# 'R' = 0x06
-# 'S' = 0x07, therfore... 'SL' = 0x07-0x05 = 0x02, 'SR' = 0x01
+
+bitstype 8 EdgeType
+const EDGETYPE_TO_UINT8 = fill( 0x07, (8,8) )
+EDGETYPE_TO_UINT8[8,6] = 0x00 # 'SL' = 0x00 # Tx Start
+EDGETYPE_TO_UINT8[8,7] = 0x01 # 'SR' = 0x01 # Tandem Last Exon
+EDGETYPE_TO_UINT8[6,8] = 0x02 # 'LS' = 0x02 # Tandem First Exon
+EDGETYPE_TO_UINT8[7,8] = 0x03 # 'RS' = 0x03 # Tx End
+EDGETYPE_TO_UINT8[6,7] = 0x04 # 'LR' = 0x04 # Intron 5'->3' SS
+EDGETYPE_TO_UINT8[6,6] = 0x05 # 'LL' = 0x05 # Alt- 5'SS
+EDGETYPE_TO_UINT8[7,7] = 0x06 # 'RR' = 0x06 # Alt- 3'SS
+
+function Base.convert( ::Type{EdgeType}, one::UInt8, two::UInt8 )
+   @assert( 5 <= one <= 7 && 5 <= one <= 7 ) 
+   EDGETYPE_TO_UINT8[one+1,two+1]
+end
+
+Base.convert( ::Type{EdgeType}, one::DNANucleotide, two::DNANucleotide ) = Base.convert( EdgeType, convert(UInt8, one), convert(UInt8, two) )
+Base.convert( ::Type{EdgeType}, edge::UInt8 ) = box(EdgeType, unbox(UInt8, edge ))
+Base.convert( ::Type{UInt8}, edge::EdgeType ) = box(UInt8, unbox(EdgeType, edge ))
 
 # This is where we count reads for nodes/edges/circular-edges
 type SpliceGraphQuant 
@@ -24,10 +39,8 @@ SpliceGraphQuant() = SpliceGraphQuant( Vector{Float64}(), Vector{UInt32}(),
 # which is a directed multigraph
 immutable SpliceGraph
   nodeoffset::Vector{Coordint}
-  edgeoffset::Vector{Coordint}
-  left::Vector{Exonmax} #donor splice sites indexes
-  right::Vector{Exonmax} #acceptor splice site indexes
-  txend::Vector{Exonmax}  #tx starts/ends
+  nodelen::Vector{Coordint}
+  edgetype::Vector{EdgeType}
   seq::DNASequence
 end
 
@@ -41,16 +54,16 @@ SpliceGraph() = SpliceGraph( Vector{Coordint}(), Vector{Coordint}(),
 function SpliceGraph( gene::Refgene, chrom::DNASequence )
    # splice graph variables
    nodeoffset = Vector{Coordint}()
-   nodesize   = Vector{Coordint}()
-   edgeoffset = Vector{Coordint}()
-   edgetype   = Vector{Edgetype}()
+   nodelen    = Vector{Coordint}()
+   edgetype   = Vector{EdgeType}()
    seq        = DNASequence()
    
+   a = length(gene.acc)
+   d = length(gene.don)
 
    for i in 1:length(don)
       # iterate through donors, and acceptors
       # left to right. '-' = rc unshift?
    end
-   return SpliceGraph( nodeoffset, edgeoffset, 
-                       left, right, txend, seq )
+   return SpliceGraph( nodeoffset, nodelen, edgetype, seq )
 end
