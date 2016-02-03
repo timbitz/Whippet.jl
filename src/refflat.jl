356b57834697945548bb5ef67d2f65b71f2cf88c
Tim Sterne-Weiler <tim.sterne.weiler@utoronto.ca>
2016-02-03 11:14:58 -0500

    refflat edges code removed, currently only in edges.jl

--- src/refflat.jl
+++ src/refflat.jl
@@ -62,18 +62,6 @@ function unique_tuple{T}( tup1::Tuple{Vararg{T}}, tup2::Tuple{Vararg{T}})
    tuple(uniq...)
 end
 
-#=  Useless?
-function find_microexons{T}( don::Tuple{Vararg{T}}, acc::Tuple{Vararg{T}})
-   mics = Vector{Tuple{T,T,T}}()
-   for i in 1:(length(acc)-1)
-      exonlen = don[i+1] - acc[i]
-      if exonlen < 16   #TODO make sure 1-based and 0-based coordinate math works!
-         push!(mics, (acc[i], don[i+1], convert(T, i)))
-      end
-   end
-   mics
-end
-=#
 
 # Load refflat file from filehandle
 # Refflat format must be as expected from output of gtfToGenePred -ext
@@ -179,57 +167,6 @@ function load_refflat( fh; txbool=false )
 end
 
 
-#### Splice k-mer graphs.  MOVE TO NEW FILE
-immutable Acceptor{K}
-   kmer::DNAKmer{K}
-   microexon::UInt8
-   gene::Genename
-end
-
-function setacceptor!{K}( donorvec::Vector, left::DNAKmer{K}, right::DNAKmer{K}, gene::Genename )
-   lind = Int(UInt64(left)) + 1
-   if !isdefined( donorvec, lind )
-      donorvec[lind] = Vector{Acceptor{K}}()
-   end
-   push!(donorvec[lind], Acceptor{K}(right, UInt8(0), gene) )
-end
-
-function push_or_set!{T}( array::Vector, index::DNAKmer, value::T)
-   ind = Int(UInt64(index)) + 1
-   if !isdefined( array, ind )
-      array[ind] = Set{T}()
-   end
-   push!( array[ind], value )
-end
-
-function Base.intersect{T}( tup1::Tuple{Vararg{T}}, tup2::Tuple{Vararg{T}} )
-   res = Vector{T}()
-   for a in tup1, b in tup2
-      if a == b
-         push!(res, a)
-      end
-   end
-   res
-end
-
-# Intersect two sorted arrays.
-function intersect_sorted{T}( arrA::Vector{T}, arrB::Vector{T} )
-   res = Vector{T}()
-   i,j = 1,1
-   while i <= length(arrA) && j <= length(arrB)
-      if arrA[i] > arrB[j]
-         j += 1
-      elseif arrA[i] < arrB[j]
-         i += 1
-      else
-         push!( res, arrA[i] )
-         i += 1
-         j += 1
-      end
-   end
-   res
-end 
-
 # Binary search.
 function search_sorted{T}( arr::Vector{T}, elem::T, low=1, high=length(arr)+1 )
    low == high && return(-1)
@@ -243,56 +180,6 @@ function search_sorted{T}( arr::Vector{T}, elem::T, low=1, high=length(arr)+1 )
    ret
 end
 
-# build k-mer arrays for splice junctions
-function donor_junc_table( genome, ref, k::Integer )
-   donors = Vector{Vector{Genename}}(4^k)
-   accept = Vector{Vector{Genename}}(4^k)
-
-   for gene in keys(ref.genetotx)
-      chrom,strand = ref.gninfo[gene]
-      offset = getoffset( genome, chrom )
-      (offset == -1) && continue # chrom not present
-
-      function build_kmer_gene( coords::Tuple, kmers::Vector; ladj=0, radj=0 )
-         for c in coords
-            coor = c+offset
-            try
-               curkmer = DNAKmer{k}(genome.seq[(coor+ladj):(coor+radj)])
-               ind = Int(UInt64(curkmer))+1
-               if !isdefined(kmers, ind)
-                  kmers[ind] = Vector{typeof(gene)}()
-               end
-               push!(kmers[ind], gene)
-               kmers[ind] = sort(kmers[ind]) |> unique
-            catch e
-               #println(e) # these should just be N containing
-               continue
-            end
-         end
-      end
-      build_kmer_gene( ref.gndon[gene], donors, ladj=(1-k) )
-      build_kmer_gene( ref.gnacc[gene], accept, radj=(k-1) )
-
-   end
-   donors,accept
-end
-      #=
-      for d in ref.gndon[gene]#, a in ref.gnacc[gene]
-         don = d+offset#,a+offset
-         try
-            left = DNAKmer{k}(genome.seq[(don-k+1):don])
-            #right = DNAKmer{k}(genome.seq[acc:(acc+k-1)])
-            #push_or_set!( donors, left, gene )
-            lind = Int(UInt64(left))+1
-            donors[lind] = isdefined(donors, lind) ? unique_tuple(donors[lind], tuple(gene)) : tuple(gene)
-         catch e
-            println(e)
-            continue
-         end
-         #println("$gene $chrom $strand $d $k $curk")
-         #print(">$chrom-$don-$acc\n$(convert(AbstractString,left)*convert(AbstractString,right))\n")
-      end
-      =#
 
 function main()
    println(STDERR, "Loading Refflat file...")
@@ -317,7 +204,6 @@ function main()
    gc()
    return ref  
 
-   include("index.jl")
    println(STDERR, "Loading genome index...")
    @time genome = open(deserialize, "$(pwd())/index/genome.jls")
    @time junclib_d,junclib_a = donor_junc_table( genome, ref, 9 )
