d21e62918d2eadc806c3fc6dc64f9de5fd795504
Tim Sterne-Weiler <tim.sterne.weiler@utoronto.ca>
2016-02-03 01:26:43 -0500

    added work on edges.jl, still needs RR LL LR removal work

--- src/edges.jl
+++ src/edges.jl
@@ -28,11 +28,35 @@ function is_edge( edge::EdgeType, left::Bool )
 end
 
 function add_kmer_edge!{S <: NucleotideSequence}( kmers::Vector{SGNodeSet}, 
-                                                  seq::S, l, r, entry::SGNodeTup )
+                                                  seq::S, l, r, left::Bool,
+                                                  entry::SGNodeTup )
+   s = seq[l:r]
+   ind = 0 #default
+   #println( "$(seq[(l-4):(l-1)]) + $(seq[l:r]) + $(seq[(r+1):(r+4)])" )
    try
-      println( "$(seq[(l-4):(l-1)]) + $(seq[l:r]) + $(seq[(r+1):(r+4)])" )
-      curkmer = DNAKmer( seq[l:r] )
+      curkmer = SGKmer( s ) 
       ind = Int(UInt64(curkmer)) + 1
+   catch
+      println( "Caught!!" )
+      sub = ""
+      do
+         sub = split( AbstractString(s), r"LL|LR|RR" )
+         if length(sub) > 1
+            res = join( sub, "" )
+            s = left ? AbstractString(seq[(l-2):(l-1)]) * res :
+                       res * AbstractString(seq[(r+1):(r+2)])
+         else
+            break
+         end
+         println( "Removed splice site to get $s size!" )
+      end
+      try
+        curkmer = SGKmer( s )
+        ind = Int(UInt64(curkmer)) + 1
+        println( "Index size: $ind, kmer size: $(typeof(curkmer))" )
+      end
+   end
+   if ind > 0
       if !isdefined(kmers, ind)
          kmers[ind] = SGNodeSet()
       end
@@ -50,10 +74,10 @@ function build_edges( graphs::Vector{SpliceGraph}, k::Integer )
       for (j,n) in enumerate(g.nodeoffset)
 
          if is_edge( g.edgetype[j], true ) # left edge
-            add_kmer_edge!( left, g.seq, n-k-2, n-3, SGNodeTup(i,j) )
+            add_kmer_edge!( left, g.seq, n-k-2, n-3, true,  SGNodeTup(i,j) )
          end
          if is_edge( g.edgetype[j], false ) # right edge
-            add_kmer_edge!( right, g.seq, n,  n+k-1, SGNodeTup(i,j) )
+            add_kmer_edge!( right, g.seq, n,  n+k-1, false, SGNodeTup(i,j) )
          end
 
       end
