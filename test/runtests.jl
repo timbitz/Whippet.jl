a391ef95685159eff8b03a23a6202d07e826d12e
Tim Sterne-Weiler <tim.sterne.weiler@utoronto.ca>
2016-02-09 11:30:26 -0500

    added test dir

---
+++ test/runtests.jl
@@ -0,0 +1,19 @@
+using FactCheck
+
+facts("Gene Annotation") do
+
+end
+facts("Splice Graph") do
+   facts("Edge types") do 
+ 
+   end
+   facts("Building graph") do
+
+   end
+end
+facts("Kmer Edges") do
+
+end
+facts("Alignment") do
+
+end
