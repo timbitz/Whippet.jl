11f68ec40fb699dfcc39bfe2ea04c7689b396f7c
Tim Sterne-Weiler <tim.sterne.weiler@utoronto.ca>
2016-02-18 13:28:19 -0500

    initial rec_gene_em! implementation, untested

--- src/quant.jl
+++ src/quant.jl
@@ -43,19 +43,24 @@ function GraphLibQuant( lib::GraphLib, ref::Refset )
    GraphLibQuant( tpm, count, len, quant )
 end
 
-function calculate_tpm!( quant::GraphLibQuant, counts::Vector{Float64}=quant.count; readlen=50 )
+function calculate_tpm!( quant::GraphLibQuant, counts::Vector{Float64}=quant.count; readlen=50, sig=0 )
    for i in 1:length(counts)
       quant.tpm[ i ] = counts[i] * readlen / quant.length[i]
    end
    const rpk_sum = sum( quant.tpm )
    for i in 1:length(quant.tpm)
-      quant.tpm[i] = ( quant.tpm[i] * SCALING_FACTOR / rpk_sum )
+      if sig > 0
+         quant.tpm[i] = signif( quant.tpm[i] * SCALING_FACTOR / rpk_sum, sig )
+      else
+         quant.tpm[i] = ( quant.tpm[i] * SCALING_FACTOR / rpk_sum )
+      end
    end
 end
 
-immutable Multimap
+type Multimap
    align::Vector{SGAlignment}
    prop::Vector{Float64}
+   prop_sum::Float64
 end
 
 Multimap( align::SGAlignment ) = length(align.path) >= 1 ? 
@@ -91,9 +96,49 @@ function count!( graphq::GraphLibQuant, align::SGAlignment; val=1.0 )
    end
 end
 
+function unsafe_copy!{T <: Number}( dest::Vector{T}, src::Vector{T} )
+   for i in 1:length(src)
+      dest[i] = src[i]
+   end
+end 
 
+# This function recursively performs expectation maximization
+# and sets the quant.tpm array as the proposed expression set
+# at each iteration.   It also requires that calculate_tpm! be 
+# run initially once prior to rec_gene_em!() call.
 @fastmath function rec_gene_em!( quant::GraphLibQuant,
                                  ambig::Vector{Multimap}, 
+                                 temp::Vector{Float64}=ones(length(quant.count)),
+                                 uniqsum=sum(quant.count),
+                                 ambigsum=length(ambig),
                                  it=1, max=1000, sig=0)
-    
+   
+   unsafe_copy!( temp, quant.count )
+   for mm in ambig
+      if it > 1 # Maximization
+         mm.prop_sum = 0.0
+         for ai in 1:length(mm.align)
+            init_gene = mm.align[ai].path[1].gene
+            init_tpm  = quant.tpm[ init_gene ]
+            mm.prop[ai] = init_tpm
+            mm.prop_sum += init_tpm
+         end
+      end
+      
+      for ai in 1:length(mm.align)
+         init_gene = mm.align[ai].path[1].gene
+         temp[ init_gene ] += (mm.prop[ai] / mm.prop_sum)
+      end
+   end
+
+   calculate_tpm!( quant, temp, sig=sig ) # Expectation
+     
+   #iterate
+   if temp != quant.tpm && it < max
+      it = rec_gene_em( quant, ambig, temp, uniqsum=uniqsum, ambigsum=ambigsum, 
+                        it=it+1, max=max, sig=sig)
+   end
+   it
 end
+
+
