83ce15ce94c5dca5b4672a21463b1118118ebc51
Tim Sterne-Weiler <tim.sterne.weiler@utoronto.ca>
2016-02-21 00:30:06 -0500

    added convert and isobligate to events.jl

--- src/events.jl
+++ src/events.jl
@@ -4,8 +4,6 @@ bitstype 8 EdgeMotif
 Base.convert( ::Type{EdgeMotif}, motif::UInt8 ) = box(EdgeMotif, unbox(UInt8, motif ))
 Base.convert( ::Type{UInt8}, motif::EdgeMotif ) = box(UInt8, unbox(EdgeMotif, motif ))
 
-const NONE_MOTIF = convert(EdgeMotif, 0b1000)
-
 # OBLIGATE MOTIFS (provided the cognate nodes both have reads):
 
 const TXST_MOTIF = convert(EdgeMotif, 0b000)
@@ -22,40 +20,46 @@ const SKIP_MOTIF = convert(EdgeMotif, 0b101)
 const ALTD_MOTIF = convert(EdgeMotif, 0b110)
 const ALTA_MOTIF = convert(EdgeMotif, 0b111)
 
-const MOTIF_TABLE = fill(NONE_MOTIF, 128 )
+# NO MOTIF
+const NONE_MOTIF = convert(EdgeMotif, 0b1000)
+
+const MOTIF_TABLE = fill(NONE_MOTIF, 2^6 )
       # Alt TxStart  SL SL
-      MOTIF_TABLE[ 0b000000 ] = TXST_MOTIF
+      MOTIF_TABLE[ 0b000000 + 1 ] = TXST_MOTIF
       # Alt PolyA    RS RS
-      MOTIF_TABLE[ 0b011011 ] = TXEN_MOTIF
+      MOTIF_TABLE[ 0b011011 + 1 ] = TXEN_MOTIF
 
       # Alt FirstEx  SL LS
-      MOTIF_TABLE[ 0b000010 ] = ALTF_MOTIF
+      MOTIF_TABLE[ 0b000010 + 1 ] = ALTF_MOTIF
       # Alt LastEx   SR RS
-      MOTIF_TABLE[ 0b001011 ] = ALTL_MOTIF
+      MOTIF_TABLE[ 0b001011 + 1 ] = ALTL_MOTIF
 
       # RetainedInt  LL RR
-      MOTIF_TABLE[ 0b101110 ] = RETI_MOTIF
+      MOTIF_TABLE[ 0b101110 + 1 ] = RETI_MOTIF
 
       # Skipped      RR LL
-      MOTIF_TABLE[ 0b110101 ] = SKIP_MOTIF
+      MOTIF_TABLE[ 0b110101 + 1 ] = SKIP_MOTIF
       #              RR LR
-      MOTIF_TABLE[ 0b110100 ] = SKIP_MOTIF
+      MOTIF_TABLE[ 0b110100 + 1 ] = SKIP_MOTIF
       #              LR LL
-      MOTIF_TABLE[ 0b100101 ] = SKIP_MOTIF
+      MOTIF_TABLE[ 0b100101 + 1 ] = SKIP_MOTIF
       #              LR LR
-      MOTIF_TABLE[ 0b100100 ] = SKIP_MOTIF
+      MOTIF_TABLE[ 0b100100 + 1 ] = SKIP_MOTIF
 
       # Alt Donor    LL LL
-      MOTIF_TABLE[ 0b101101 ] = ALTD_MOTIF
+      MOTIF_TABLE[ 0b101101 + 1 ] = ALTD_MOTIF
       #              LL LR
-      MOTIF_TABLE[ 0b101100 ] = ALTD_MOTIF
+      MOTIF_TABLE[ 0b101100 + 1 ] = ALTD_MOTIF
 
       # Alt Acceptor RR RR
-      MOTIF_TABLE[ 0b110110 ] = ALTA_MOTIF
+      MOTIF_TABLE[ 0b110110 + 1 ] = ALTA_MOTIF
       #              LR RR
-      MOTIF_TABLE[ 0b100110 ] = ALTA_MOTIF
+      MOTIF_TABLE[ 0b100110 + 1 ] = ALTA_MOTIF
 
-Base.convert(::Type{EdgeMotif}, tup::Tuple{EdgeType,EdgeType}) = MOTIF_TABLE[ (UInt8(tup[1]) << 3) | UInt8(tup[2]) ]
+Base.convert(::Type{EdgeMotif}, tup::Tuple{EdgeType,EdgeType}) = Base.convert(EdgeMotif, tup... )
+Base.convert(::Type{EdgeMotif}, current::EdgeType, next::EdgeType) = MOTIF_TABLE[ (UInt8(current) << 3) | UInt8(next) + 1 ]
+
+isobligate( motif::EdgeMotif ) = motif != NONE_MOTIF && !( UInt8(motif) & 0b100 == 0b100 )
 
 function process_sgquant( lib::GraphLib, graphq::GraphLibQuant )
    for g in 1:length(lib.graphs)
@@ -78,7 +82,12 @@ function process_events( sg::SpliceGraph, sgquant::SpliceGraphQuant )
       return 0
    end
    for i in 1:length(sg.edges)-1
-      if obligate( sg.edges[i], sg.edges[i+1] )
-            
+      motif = convert(EdgeMotif, sg.edges[i], sg.edges[i+1] )
+      motif == NONE_MOTIF && continue
+      if isobligate( motif )
+         # we have to calculate something for sure
+      else
+         # Check for spanning edge 
+      end  
    end
 end
