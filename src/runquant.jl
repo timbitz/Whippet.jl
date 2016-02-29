15d6a7e93c902eb93c84d6987613beed646ed470
Tim Sterne-Weiler <tim.sterne.weiler@utoronto.ca>
2016-02-29 13:29:56 -0500

    fixed runquant.jl index loading defaults

--- src/runquant.jl
+++ src/runquant.jl
@@ -17,7 +17,9 @@ using ArgParse
 #include("quant.jl")
 #include("reads.jl")
 
-push!( LOAD_PATH, splitdir(@__FILE__)[1] )
+dir = splitdir(@__FILE__)[1]
+
+push!( LOAD_PATH, dir )
 import SpliceGraphs
 @everywhere using SpliceGraphs
 
@@ -45,7 +47,7 @@ function parse_cmd()
     "--index", "-x"
       help = "Output prefix for saving index 'dir/prefix' (default Whippet/index/graph)"
       arg_type = ASCIIString
-      default = "$(pwd())/../index/graph"
+      default = "$(dir)/../index/graph"
   end
   return parse_args(s)
 end
@@ -54,11 +56,11 @@ function main()
 
    args = parse_cmd()
 
-   println(STDERR, "Loading splice graph index...")
-   @time const lib = open(deserialize, "$(pwd())/../index/graph.jls")
+   println(STDERR, "Loading splice graph index... $( args["index"] ).jls")
+   @time const lib = open(deserialize, "$( args["index"] ).jls")
 
-   println(STDERR, "Loading annotation index...")
-   @time const anno = open(deserialize, "$(pwd())/../index/graph_anno.jls")
+   println(STDERR, "Loading annotation index... $( args["index"] )_anno.jls")
+   @time const anno = open(deserialize, "$( args["index"] )_anno.jls")
 
    const param = AlignParam() # defaults for now
    const quant = GraphLibQuant( lib, anno )
