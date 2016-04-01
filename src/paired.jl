a8756dc2d0bf82cdd674ac75e3f9bc6b660f3e5f
Tim Sterne-Weiler <tim.sterne.weiler@utoronto.ca>
2016-03-31 20:00:26 -0400

    mid implementation of paired-end alignment

--- src/paired.jl
+++ src/paired.jl
@@ -1,48 +1,14 @@
 
-function seed_intersect( fwd_sa::UnitRange, rev_sa::UnitRange, index::FMIndexes.FMIndex, max_dist::Int )
-   
-end
 
-# paired-end seed_locate
-function seed_locate( p::AlignParam, index::FMIndex, fwd::SeqRecord, rev::SeqRecord; fwd_offset_left=true, rev_offset_left=false )
-   const def_sa = 2:1
-   ctry = 1
-
-   if fwd_offset_left
-      const fwd_increment = p.seed_inc
-      fwd_curpos = p.seed_buffer
-   else
-      const fwd_increment = -p.seed_inc
-      fwd_curpos = length(fwd.seq) - p.seed_length - p.seed_buffer
+@inline function sorted_offsets( sa::UnitRange, index::FMIndexes.FMIndex )
+   res = Vector{Int}(length(sa))
+   ridx = 1
+   for sidx in sa
+      res[ridx] = FMIndexes.sa_value( seed[sidx], lib.index ) + 1
+      ridx += 1
    end
-
-   if rev_offset_left
-      const rev_increment = p.seed_inc
-      rev_curpos = p.seed_buffer
-   else
-      const rev_increment = -p.seed_inc
-      rev_curpos = length(rev.seq) - p.seed_length - p.seed_buffer
-   end
-   
-   const fwd_maxright = length(fwd.seq) - p.seed_length - p.seed_buffer
-   const rev_maxright = length(rev.seq) - p.seed_length - p.seed_buffer
-
-   while( ctry <= p.seed_try && p.seed_buffer <= fwd_curpos <= fwd_maxright && 
-                                p.seed_buffer <= rev_curpos <= rev_maxright )
-
-      const fwd_sa = FMIndexes.sa_range( fwd.seq[fwd_curpos:(fwd_curpos+p.seed_length-1)], index )
-      const rev_sa = FMIndexes.sa_range( rev.seq[rev_curpos:(rev_curpos+p.seed_length-1)], index )
-
-      const cnt = length(sa)
-      ctry += 1
-      #println("$sa, curpos: $curpos, cnt: $cnt, try: $(ctry-1)")
-      if cnt == 0 || cnt > p.seed_tolerate
-         curpos += increment
-      else
-         return sa,curpos
-      end
-   end
-   def_sa,curpos
+   sort!(res)
+   res
 end
 
 function ungapped_align( p::AlignParam, lib::GraphLib, fwd::SeqRecord, rev::SeqRecord; ispos=true, anchor_left=true )
@@ -52,53 +18,81 @@ function ungapped_align( p::AlignParam, lib::GraphLib, fwd::SeqRecord, rev::SeqR
    if p.is_pair_rc
       reverse_complement!(rev.seq)
       reverse!(rev.metadata.quality)
-      const rev_seed,rev_readloc = seed_locate( p, lib.index, rev, offset_left=false )
+      const rev_seed,rev_readloc = seed_locate( p, lib.index, rev, offset_left=!anchor_left )
    else
       const rev_seed,rev_readloc = seed_locate( p, lib.index, rev, offset_left=anchor_left )
    end
 
-   res      = Nullable{Vector{SGAlignment}}()
+   fwd_res      = Nullable{Vector{SGAlignment}}()
+   rev_res      = Nullable{Vector{SGAlignment}}()
    maxscore = 0.0
-   for sidx in 1:length(seed)
-      const s = FMIndexes.sa_value( seed[sidx], lib.index ) + 1
-      const geneind = search_sorted( lib.offset, convert(Coordint, s), lower=true )
-      align = ungapped_fwd_extend( p, lib, convert(Coordint, geneind),
-                                   s - lib.offset[geneind] + p.seed_length,
-                                   read, readloc + p.seed_length, ispos=ispos )
 
-      align = ungapped_rev_extend( p, lib, convert(Coordint, geneind),
-                                   s - lib.offset[geneind] - 1,
-                                   read, readloc - 1, ispos=ispos, align=align,
-                                   nodeidx=align.path[1].node )
-      if align.isvalid
-         if isnull( res )
-            res = Nullable(SGAlignment[ align ])
+   const fwd_sorted = sorted_offsets( fwd_seed, lib.index )
+   const rev_sorted = sorted_offsets( rev_seed, lib.index )
+
+   fidx,ridx = 1,1
+
+   while fidx <= length(fwd_sorted) && ridx <= length(rev_sorted)
+
+      const dist = abs( fwd_sorted[fidx] - rev_sorted[ridx] )
+      if dist > p.seed_pair_range # outside allowable pair distance
+         if fwd_sorted[fidx] > rev_sorted[ridx]
+            ridx += 1
          else
-            # new best score
-            const scvar = score(align)
-            if scvar > maxscore
-               # better than threshold for all of the previous scores
-               if scvar - maxscore > p.score_range
-                  length( res.value ) >= 1 && empty!( res.value )
-               else # keep at least one previous score
-                  splice_by_score!( res.value, scvar, p.score_range )
-               end
-               maxscore = scvar
-               push!( res.value, align )
-            else # new score is lower than previously seen
-               if maxscore - scvar <= p.score_range # but tolerable
-                  push!( res.value, align )
-               end
-            end # end score vs maxscore
-         end # end isnull 
-      end # end isvalid 
+            fidx += 1
+         end
+         continue
+      else
+         fwd_res = _ungapped_align( p, lib, fwd_res, fwd, fwd_sorted[fidx] )
+         rev_res = _ungapped_align( p, lib, rev_res, rev, rev_sorted[ridx] )
+      end
+
    end
    # if !stranded and no valid alignments, run reverse complement
-   if ispos && !p.is_stranded && isnull( res )
-#      read.seq = Bio.Seq.unsafe_complement!(Bio.Seq.reverse( read.seq ))
-      reverse_complement!( read.seq )
-      reverse!( read.metadata.quality )
-      res = ungapped_align( p, lib, read, ispos=false, anchor_left=!anchor_left )
+   if ispos && !p.is_stranded && (isnull( fwd_seed ) || isnull( rev_seed ))
+      reverse_complement!( fwd.seq )
+      reverse_complement!( rev.seq )
+      reverse!( fwd.metadata.quality )
+      reverse!( rev.metadata.quality )
+      fwd_res,rev_res = ungapped_align( p, lib, fwd, rev, ispos=false, anchor_left=!anchor_left )
    end
+
+   fwd_res,rev_res
+end
+
+@inline function _ungapped_align( p::AlignParam, lib::GraphLib, res::Nullable{Vector{SGAlignment}}, read::SeqRecord, indx::Int )
+
+   const geneind = search_sorted( lib.offset, convert(Coordint, indx), lower=true )
+   align = ungapped_fwd_extend( p, lib, convert(Coordint, geneind),
+                                indx - lib.offset[geneind] + p.seed_length,
+                                read, readloc + p.seed_length, ispos=ispos )
+
+   align = ungapped_rev_extend( p, lib, convert(Coordint, geneind),
+                                indx - lib.offset[geneind] - 1,
+                                read, readloc - 1, ispos=ispos, align=align,
+                                nodeidx=align.path[1].node )
+
+   if align.isvalid
+      if isnull( res )
+         res = Nullable(SGAlignment[ align ])
+      else
+            # new best score
+         const scvar = score(align)
+         if scvar > maxscore
+            # better than threshold for all of the previous scores
+            if scvar - maxscore > p.score_range
+               length( res.value ) >= 1 && empty!( res.value )
+            else # keep at least one previous score
+               splice_by_score!( res.value, scvar, p.score_range )
+            end
+            maxscore = scvar
+            push!( res.value, align )
+         else # new score is lower than previously seen
+            if maxscore - scvar <= p.score_range # but tolerable
+               push!( res.value, align )
+            end
+         end # end score vs maxscore
+      end # end isnull 
+   end # end isvalid 
    res
 end
