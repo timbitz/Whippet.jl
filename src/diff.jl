9c993659e62ffeeea6b84f9b52910b66f90d36c2
Tim Sterne-Weiler <tim.sterne.weiler@utoronto.ca>
2016-05-02 17:26:50 -0400

    initial buffered stream opening for diff sample replicates

--- src/diff.jl
+++ src/diff.jl
@@ -1,33 +1,51 @@
 
-
 immutable PosteriorPsi
    beta::Distributions.Beta
-   sample::Vector{Float64}
+   emperical::Vector{Float64}
    psi::Float64
 end
 
 function PosteriorPsi( psi::Float64, N::Float64, α=1.0, β=1.0; size=1000 )
    # Posterior Beta is k + α, n-k + β
    beta = Beta( psi*N + α, (1-psi)*N + β )
-   sample = rand(beta, size)
+   emperical = rand(beta, size)
    psi = mean(beta)
-   PosteriorPsi( beta, sample, psi )
+   PosteriorPsi( beta, emperical, psi )
 end
 
 # combine multiple beta posteriors for replicates into one
 function PosteriorPsi( set::Vector{PosteriorPsi} )
-   sample = vcat( map( x->x.sample, set )... )
-   beta = fit(Beta, sample)
+   emperical = vcat( map( x->x.emperical, set )... )
+   beta = fit(Beta, emperical)
    psi = mean(beta)
-   PosteriorPsi( beta, sample, psi )
+   PosteriorPsi( beta, emperical, psi )
 end
 
-# Calculates the probability, P(|a-b| > amt)
+# Calculates the probability, P( a-b > amt)
 function probability( a::PosteriorPsi, b::PosteriorPsi; amt=0.0 )
    good = 0
-   total = min(length(a.sample),length(b.sample))
+   total = min(length(a.emperical),length(b.emperical))
    for i in 1:total
-      good += abs(a.sample[i] - b.sample[i]) > amt ? 1 : 0
+      good += a.emperical[i] - b.emperical[i] > amt ? 1 : 0
    end
    @fastmath good / total
 end
+
+function open_stream( filename )
+   fopen = open( filename, "r" )
+   if isgzipped( filename )
+      stream = ZlibInflateInputStream( fopen, reset_on_end=true )
+   else
+      stream = BufferedStreams.BufferedInputStream( fopen )
+   end
+   stream
+end
+
+function open_streams( files::Vector{ASCIIString} )
+   buf = Vector{BufferedStreams.BufferedInputStream}(length(files))
+   for i in 1:length(files)
+      buf[i] = open_stream( files[i] )
+   end
+end
+ 
+
