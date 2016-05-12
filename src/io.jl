05cdd775aa7054b510f8a40fac4dbbf3400ade92
tim <tim.sterne.weiler@utoronto.ca>
2016-05-12 14:40:46 -0400

    don't print NA BS line

--- src/io.jl
+++ src/io.jl
@@ -218,19 +218,17 @@ function output_circular( io::BufOut, sg::SpliceGraph, sgquant::SpliceGraphQuant
       end
       total_reads = (fore_cnt + back_cnt)
       psi = fore_cnt / total_reads
-      tab_write( io, info[1] )
-      coord_write( io, info[2], sg.nodecoord[st]+sg.nodelen[st]-1, sg.nodecoord[en], tab=true )
-      tab_write( io, info[3] )
-      tab_write( io, "BS" )
       if !isnan(psi) && total_reads > 0
+         tab_write( io, info[1] )
+         coord_write( io, info[2], sg.nodecoord[st]+sg.nodelen[st]-1, sg.nodecoord[en], tab=true )
+         tab_write( io, info[3] )
+         tab_write( io, "BS" )
          tab_write( io, "C1" )
          tab_write( io, string(psi) )
          conf_int  = beta_posterior_ci( psi, total_reads, sig=3 )
          conf_int_write( io, conf_int, tab=true, width=true)
          tab_write( io, string( signif(total_reads, 3) ) )
          write( io, "NA\tNA\n" )
-      else
-         write( io, "NA\tNA\tNA\tNA\tNA\tNA\tNA\n" )
       end
    end
 end
