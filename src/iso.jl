b4fb9da89c91fcd1a8691a8933919ccdf7da8cff
tim <tim.sterne.weiler@utoronto.ca>
2016-07-18 17:34:27 -0400

    initial iso.jl

---
+++ src/iso.jl
@@ -0,0 +1,26 @@
+# Extended from events.jl for full isoforms
+
+function full_graph( sgquant::SpliceGraphQuant )
+   graph = Nullable{PsiGraph}()
+   for edg in sgquant.edge
+      if isnull( graph )
+         graph = Nullable(PsiGraph( Vector{Float64}(), Vector{Float64}(),
+                                    Vector{Float64}(), Vector{IntSet}(),
+                                    edg.first, edg.last ))
+      end
+      push!( graph.value, edg, value_bool=false )
+   end
+   graph
+end
+
+function process_isoforms( sg::SpliceGraph, sgquant::SpliceGraphQuant )
+   graph = full_graph( sgquant )
+   if !isnull( graph )
+      graph = Nullable( reduce_graph( graph.value ) )
+   end
+   
+end
+
+# TODO: filter isoforms with out txStart and End
+# TODO: find largest ORF
+# TODO: 
